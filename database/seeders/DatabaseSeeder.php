aa93976103b4172e587926114f930e5078f4ddd8
Luis Henrique <luis_henrique_co@hotmail.com>
2020-09-25 12:29:04 -0300

    GH-11 Atualizando DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        $this->call([UsersTableSeeder::class, VehicleTableSeeder::class, ZoneTableSeed::class]);
+        $this->call([UsersTableSeeder::class, VehicleTableSeeder::class, ZoneTableSeed::class, ParkingTableSeeder::class]);
 
     }
 }
