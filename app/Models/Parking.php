7cfe839a5395f60463e679f5e0a46db26f2805db
Luis Henrique <luis_henrique_co@hotmail.com>
2020-09-25 12:25:31 -0300

    Criando Model do Estacionamento

---
+++ app/Models/Parking.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Parking extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'zone','input','exit','table'
+    ];
+}
