1765c207174668b48596a60eb1d1a7d7749e58a4
FerreiraLarissa <laryssaferreira12386@gmail.com>
2020-09-25 09:09:17 -0300

    GH-11 Adicionando tabela do estacionamento

---
+++ database/migrations/2020_09_25_114621_create_parkings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParkingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parkings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('zone');
+            $table->string('input');
+            $table->string('exit');
+            $table->string('table');
+            $table->timestamps();
+
+            $table->foreign('zone')->references('id')->on('zones');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parkings');
+    }
+}
