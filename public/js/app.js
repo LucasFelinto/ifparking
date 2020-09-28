9aa7e08011cfcefbe66a13834770cf3f69515345
LucasFelinto <elucasfelinto@gmail.com>
2020-09-28 19:45:41 -0300

    GH-13 Ajustes de usabilidade

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Zarka\Development\outro\ifparking\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Zarka\Development\outro\ifparking\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/lucasfelinto/Development/ifparking/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/lucasfelinto/Development/ifparking/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
