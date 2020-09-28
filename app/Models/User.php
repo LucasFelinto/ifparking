0227d53a61c47fe0ff0c1084c45b3d0855bfb8af
Patrick-Santana <zonaabissal2016@gmail.com>
2020-09-28 14:44:38 -0300

    GH-8 Corrigindo Migration de User

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','class','telephone','office','siape','registration',
+        'cpf','name', 'email', 'password','telephone', 'registration',
     ];
     /**
      * The attributes that should be hidden for arrays.
