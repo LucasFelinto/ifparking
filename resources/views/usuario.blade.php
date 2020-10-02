ed05ab5f8b3ca0975aa7507f307d63d81f51b423
erinston <erinstong@gmail.com>
2020-10-02 10:17:24 -0300

    #GH-21 adicionando  view usuario

---
+++ resources/views/usuario.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <a class="nav-link active" href="#">
+                <img src="https://avatars0.githubusercontent.com/u/49694337?s=400&u=8218fb304efa70abdd3b5e396dd34e2e1e34d7e5&v=4" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+        </a>
+        <a class="navbar-brand" href="#">IFPARKING</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" href="#">PERFIL <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#">VAGAS</a>
+                <a class="nav-link" href="#">MEU VEICULO</a>
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">SAIR</a>
+         </div>
+        </div>
+    </nav>
+
+    <div class="jumbotron">
+  <h1 class="display-4">Olá, Seja bem vindo!</h1>
+  <p class="lead">ifparking é um site onde você usuario pode ver vagas de estacionameno disponives </p>
+  <hr class="my-4">
+  <p>Confira a vaga disponivel para o seu veiculo no botão abaixo</p>
+  <a class="btn btn-dark btn-lg" href="#" role="button">Ver Vagas <div class="spinner-border text-light" role="status">
+  <span class="sr-only">Loading...</span>
+</div></a>
+
+</div>
+</body>
+</html>
