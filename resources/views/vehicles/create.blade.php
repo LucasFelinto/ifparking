b5a2372d9742b43a9add599af1ba800dcdfeb020
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2020-10-09 02:53:39 -0300

    GH-25 Criação de botão com operações possiveis para o user e seu vehicle

--- resources/views/vehicles/create.blade.php
+++ resources/views/vehicles/create.blade.php
@@ -21,6 +21,17 @@
         </div>
     </nav>
 
+    <div class="d-flex align-items-center justify-content-center">
+    <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Ações disponiveis
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="{{ route('vehicles.index')}}">Listar Veiculo</a>
+  </div>
+</div>
+</div>
+
         <div class="jumbotron">
         <div class="d-flex align-items-center justify-content-center ">  
             </div>
--- resources/views/vehicles/index.blade.php
+++ resources/views/vehicles/index.blade.php
@@ -21,6 +21,18 @@
             </div>
         </div>
     </nav>
+
+    <div class="d-flex align-items-center justify-content-center">
+    <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Ações disponiveis
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="{{ route('vehicles.create')}}">Cadastrar Veiculo</a>
+  </div>
+</div>
+</div>
+
 <div class="jumbotron">
     <div class="d-flex align-items-center justify-content-center ">
         <div class="container">
@@ -38,6 +50,7 @@
                               {{session('error')}}
                             </div>
                             @endif
+
                             <table class="table">
                 
                                     <tr style="text-align: center">
