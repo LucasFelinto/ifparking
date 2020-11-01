48f7621d2376374794c83f6896e9490d68ca3bbf
Miguel Gabriel <mgbs@discente.ifpe.edu.br>
2020-10-31 21:27:01 -0300

    Atualização de menu da view create parking

--- resources/views/parking/create.blade.php
+++ resources/views/parking/create.blade.php
@@ -14,36 +14,32 @@
  </head>
  <body>
     <nav class="navbar navbar-dark bg-dark">
-        <a class="nav-link active" href="#">
-                <img src="" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
-        </a>
-        <a class="navbar-brand" href="#">IFPARKING</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <a class="navbar-brand">IFPARKING</a>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
 
-             </div>
-            </div>
-        </nav>
-          <div class="jumbotron">
-            <div class="d-flex align-items-center justify-content-center ">
-            </div>
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-8">
-                        <div class="card">
-                            <div class="card-header text-light bg-dark" style="text-align: center">{{ __('ESTACIONAMENTO') }}</div>
-                                <div class="card-body">
-                                     <form action={{ route('parking.store') }} method="POST">
-                                        @csrf
-                                        <div class="form-group row">
-                                            <div class="col-md-6">
-                                                <select id="board" class="form-control @error('board') is-invalid @enderror" name="type_id" maxlength="10"name="type_id" id="">
-                                                    <option value="1">Carro</option>
-                                                    <option value="2">Moto</option>
-                                                    <option value="3">Bicicleta</option>
+         </div>
+        </div>
+    </nav>
+
+
+    <div class="jumbotron">
+        <div class="d-flex align-items-center justify-content-center ">
+        </div>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header text-light bg-dark" style="text-align: center">{{ __('ESTACIONAMENTO') }}</div>
+                            <div class="card-body">
+                                 <form action={{ route('parking.store') }} method="POST">
+                                    @csrf
+                                    <div class="form-group row">
+                                        <div class="col-md-6">
+                                            <select id="board" class="form-control @error('board') is-invalid @enderror" name="type_id" maxlength="10"name="type_id" id="">
+                                                <option value="1">Carro</option>
+                                                <option value="2">Moto</option>
+                                                <option value="3">Bicicleta</option>
                                             </select>
                                             <input type="text" name="name" placeholder="nome">
                                         <input type="submit" class="btn btn-secondary" value="enviar">
